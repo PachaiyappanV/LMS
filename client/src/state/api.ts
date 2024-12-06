86c1ec5ce669be3534866e485b3f974e77cf29f7
PachaiyappanV <pachaiyappan394@gmail.com>
2024-12-06 17:35:15 +0530

    feat(client): add RTK Query endpoints for courses API integration
    - Added `getCourses` endpoint to fetch a list of courses with optional category filtering:
      - Accepts a `category` parameter to retrieve filtered courses or all courses.
      - Provides caching and state invalidation using the `"Courses"` tag.
    - Added `getCourse` endpoint to fetch a specific course by its ID:
      - Accepts a course ID as a parameter.
      - Uses tag-based invalidation with `"Courses"` and the specific course ID for efficient state management.
    - Enhanced API integration to handle dynamic data fetching for courses within the application.

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,7 +1,50 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const customBaseQuery = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: any
+) => {
+  const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  });
+
+  try {
+    const result: any = await baseQuery(args, api, extraOptions);
+
+    if (result.data) {
+      result.data = result.data.data;
+    }
+    return result;
+
+    return result;
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error";
+
+    return { error: { status: "FETCH_ERROR", error: errorMessage } };
+  }
+};
 
 export const api = createApi({
+  baseQuery: customBaseQuery,
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
-  endpoints: () => ({}),
+  tagTypes: ["Courses", "Users", "UserCourseProgress"],
+  endpoints: (build) => ({
+    getCourses: build.query<Course[], { category?: string }>({
+      query: ({ category }) => ({
+        url: "course",
+        params: { category },
+      }),
+      providesTags: ["Courses"],
+    }),
+
+    getCourse: build.query<Course, string>({
+      query: (id) => `courses/${id}`,
+      providesTags: (result, error, id) => [{ type: "Courses", id }],
+    }),
+  }),
 });
+
+export const { useGetCoursesQuery, useGetCourseQuery } = api;
