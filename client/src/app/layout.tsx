743094f3681b6b9437393282e6a23af6e0d5acb9
PachaiyappanV <pachaiyappan394@gmail.com>
2024-12-08 20:40:31 +0530

    feat(client): add layout to non-dashboard pages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-
-const dmSans = DM_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-dm-sans",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,8 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${dmSans.className}`}>
-        <Providers>{children}</Providers>
+      <body>
+        <Providers>
+          <div className="root-layout">{children}</div>
+        </Providers>
       </body>
     </html>
   );
